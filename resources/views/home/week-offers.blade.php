323353dd70f338555e6d28776aec850677ca9dab
Xochitl Escamilla <xochissea@gmail.com>
2019-03-07 20:05:03 -0600

    Corregir Offers por Ofertas

--- resources/views/home/week-offers.blade.php
+++ resources/views/home/week-offers.blade.php
@@ -1,13 +1,10 @@
-@extends('layout.master')
-
-@section('content')
 
 <section id="offersDeLaSemana" class="mt-5">
       <div class="container-fluid">
         <div class="row">
           <div class="col text-center text-uppercase mb-4">
             <small>Conoce las</small>
-            <h2>offers de la semana</h2>
+            <h2>Ofertas de la semana</h2>
           </div>
         </div>
         <div class="row justify-content-around shadow-lg p-3 mb-5 bg-white rounded mx-md-5">
@@ -150,5 +147,3 @@
         </div>
       </div>
     </section>
-
-@endsection
