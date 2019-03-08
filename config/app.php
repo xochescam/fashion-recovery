c9f23dc803a75d16131a02a0cb2998a2a73a9d1c
Xochitl Escamilla <xochissea@gmail.com>
2019-03-07 21:14:19 -0600

    Agregar url

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://fashionrecovery.azurewebsites.net/'),
 
     'asset_url' => env('ASSET_URL', null),
 
