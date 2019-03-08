b38538f37baba9c5a3a85a4cd1171bd0fe74c0dd
Xochitl Escamilla <xochissea@gmail.com>
2019-03-08 09:47:11 -0600

    Validación de correo

--- resources/lang/es/validation.php
+++ resources/lang/es/validation.php
@@ -38,7 +38,7 @@ return [
     'digits_between'       => ':attribute debe tener entre :min y :max dígitos.',
     'dimensions'           => 'Las dimensiones de la imagen :attribute no son válidas.',
     'distinct'             => 'El campo :attribute contiene un valor duplicado.',
-    'email'                => ':attribute no es un correo válido',
+    'email'                => 'El campo :attribute no es un correo válido',
     'exists'               => ':attribute es inválido.',
     'file'                 => 'El campo :attribute debe ser un archivo.',
     'filled'               => 'El campo :attribute es obligatorio.',
