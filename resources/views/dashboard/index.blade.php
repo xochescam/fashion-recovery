1e3f1663c95ec6428c766dffef6a63cbfebaeca4
Xochitl Escamilla <xochissea@gmail.com>
2019-03-29 15:32:15 -0600

    Opciones del dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -8,9 +8,26 @@
 	        	<div class="col-12">
 	          		<h1 class="text-left TituloFR my-4">Panel de administración</h1>
 
-						<div class="alert alert-success" role="alert">
-							¡Hola {{ Auth::User()->Name }}!
-						</div>
+					<div class="row">
+					  <div class="col-sm-6">
+					    <div class="card">
+					      <div class="card-body">
+					        <h5 class="card-title">Closets</h5>
+					        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					        <a href="{{ url('closets') }}" class="btn btn-fr">Ver</a>
+					      </div>
+					    </div>
+					  </div>
+					  <div class="col-sm-6">
+					    <div class="card">
+					      <div class="card-body">
+					        <h5 class="card-title">Prendas</h5>
+					        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					        <a href="{{ url('items') }}" class="btn btn-fr">Ver</a>
+					      </div>
+					    </div>
+					  </div>
+					</div>
 				</div>
 			</div>
 		</div>
