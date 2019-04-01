d5988016230468c47bbe87ebf4f2be5ca7f5ae78
Xochitl Escamilla <xochissea@gmail.com>
2019-04-01 17:57:40 -0600

    Recuperar contraseña

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Http\Request;
+
+use Session;
+use Redirect;
 
 class ForgotPasswordController extends Controller
 {
@@ -29,4 +34,31 @@ class ForgotPasswordController extends Controller
     {
         $this->middleware('guest');
     }
+
+
+    /**
+     * Validate the email for the given request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
+    protected function validateEmail(Request $request)
+    {
+        $request->validate(['email' => 'required|email']); //exists
+    }
+
+    /**
+     * Get the response for a successful password reset link.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetLinkResponse(Request $request, $response)
+    {
+        Session::flash('success','Correo enviado exitosamente');
+        return Redirect::to('password/reset');
+    }
+
+
 }
