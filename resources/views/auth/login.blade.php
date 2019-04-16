9dee9dd4be79ebda547d58e3feab5c1b2a2c7f70
JL Bautista <joseluis.bautista@ujed.mx>
2019-04-16 13:48:27 -0500

    Cambios al css para alinear opciones de contraseña y registro en Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,7 +34,7 @@
 
                  <div class="form-group col-md-12">
                   <label for="password">Contraseña</label>
-                  <input type="password" name="password" class="form-control" id="password" placeholder="Ingresa tu contraseña" required> 
+                  <input type="password" name="password" class="form-control" id="password" placeholder="Ingresa tu contraseña" required>
 
                   @if ($errors->has('password'))
                     <div class="invalid-validation">
@@ -53,12 +53,14 @@
 
           </div>
         </div>
-        <div class="row text-center my-5">
-          <div class="col ">
-            <p><a href="{{ route('password.request') }}" class="text-center">¿Olvidaste tu contraseña?</a></p>
+        <div class="row mt-4">
+          <div class="col col-md-6 offset-md-3">
+            <p class="mb-1"><a href="{{ route('password.request') }}" class="text-center">¿Olvidaste tu contraseña?</a></p>
           </div>
-          <div class="col ">
-            <p>¿Aún no tienes cuenta?<a href="{{ url('register',0) }}" class="text-center"> Registrate aquí</a></p>
+        </div>
+        <div class="row">
+          <div class="col col-md-6 offset-md-3">
+            <p class="mb-1">¿Aún no tienes cuenta?<a href="{{ url('register',0) }}" class="text-center"> Registrate aquí</a></p>
           </div>
         </div>
       </div>
