a67a3eb992d4992e3fcf2b8ba192a0ce8d36729e
Xochitl Escamilla <xochissea@gmail.com>
2019-04-18 11:14:27 -0500

    Título al crear vendedor

--- resources/views/seller/create.blade.php
+++ resources/views/seller/create.blade.php
@@ -6,7 +6,7 @@
       <div class="container py-5">
         <div class="row">
           <div class="col-md-8 offset-md-2">
-            <h2 class="text-center mb-5">Registro de vendedor</h2>
+            <h2 class="text-center TituloFR my-4 mb-5">Registro de vendedor</h2>
 
             <form method="POST" action="{{ url('seller') }}" enctype="multipart/form-data" class="needs-validation" novalidate>
               @include('seller.form')
