57bd53be9e05b4f9fefe2d1131931ec533dd91d6
Xochitl Escamilla <xochissea@gmail.com>
2019-04-26 12:55:23 -0500

    Dirección de compra

---
+++ app/Http/Controllers/ShippingAddController.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Auth;
+use DB;
+use Session;
+use Redirect;
+
+class ShippingAddController extends Controller
+{
+    protected $table = 'fashionrecovery.GR_002';
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validator($request);
+
+        DB::beginTransaction();
+
+        try {
+
+            $data = $this->data($request->toArray());
+
+            DB::table($this->table)->insert($data);
+        
+            DB::commit();
+
+            Session::flash('success','Se ha guardado correctamente la dirección de envio.');
+            return Redirect::to('auth/'.Auth::User()->id); //cambiar
+
+        } catch (\Exception $ex) {
+
+            DB::rollback();
+
+            Session::flash('warning','Ha ocurrido un error, inténtalo nuevamente.');
+            return Redirect::to('auth/'.Auth::User()->id);
+        }
+    }
+
+    protected function data($data) {
+
+        return [
+             'UserID'          => Auth::User()->id,
+             'Alias'           => $data['Alias'],
+             'Street'          => $data['Street'],
+             'Suburb'          => $data['Suburb'],
+             'ZipCode'         => $data['ZipCode'],
+             'State'           => $data['State'],
+             'City'            => $data['City'],
+             'PhoneContact'    => $data['PhoneContact'],
+             'References'      => $data['References'],
+             'CreationDate'    => date("Y-m-d H:i:s"),
+             'Active'          => true, //
+             'IsDefault'       => true, //
+        ];
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(Request $request)
+    {        
+        $request->validate([
+             'Alias'           => ['max:30'],
+             'Street'          => ['max:50'],
+             'Suburb'          => ['max:50'],
+             'ZipCode'         => ['numeric','max:6'],
+             'State'           => ['max:25'],
+             'City'            => ['max:25'],
+             'PhoneContact'    => ['numeric','max:13'],
+             'References'      => ['max:100'],
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validator($request);
+
+        DB::beginTransaction();
+
+        try {
+
+            $data = $this->data($request->toArray());
+
+            DB::table($this->table)
+                ->where('ShippingAddID',$id)
+                ->update($data);
+        
+            DB::commit();
+
+            Session::flash('success','Se han modificado correctamente los datos de facturación.');
+            return Redirect::to('auth/'.Auth::User()->id); //cambiar
+
+        } catch (\Exception $ex) {
+
+            DB::rollback();
+
+            Session::flash('warning','Ha ocurrido un error, inténtalo nuevamente.');
+            return Redirect::to('auth/'.Auth::User()->id);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/auth/partials/shipping-data.blade.php
@@ -0,0 +1,163 @@
+<div class="card mb-4">
+  <h5 class="card-header">Dirección de envio</h5>
+
+  <div class="card-body">
+
+    <form method="POST" action="{{ url('shipping',$shipping == null ? '' : $shipping->ShippingAddID)  }}" class="needs-validation" novalidate>
+       @csrf
+
+      @if($shipping == null)
+
+        <div class="alert alert-warning w-100" role="alert">
+          No has actualizado tu dirección de envío. 
+
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+
+        </div>
+      @endif
+        
+      <div class="form-group row">
+        <label for="Alias" class="col-sm-3 col-form-label text-right">Alias</label>
+        <div class="col-sm-9">
+          <input type="text" class="form-control" name="Alias" id="Alias" value="{{ $shipping !== null ? $shipping->Alias : old('Alias') }}" maxlength="30" required>
+
+          @if ($errors->has('Alias'))
+            <div class="invalid-validation">
+              {{ $errors->first('Alias') }}
+            </div>
+          @else
+            <div class="invalid-feedback">
+              El campo Alias es obligatorio.
+            </div>
+          @endif
+        </div>                      
+      </div>
+
+      <div class="form-group row">
+        <label for="Street" class="col-sm-3 col-form-label text-right">Calle</label>
+        <div class="col-sm-9">
+          <input type="text" class="form-control" name="Street" id="Street" value="{{ $shipping !== null ? $shipping->Street : old('Street') }}" maxlength="50" required>
+
+          @if ($errors->has('Street'))
+            <div class="invalid-validation">
+              {{ $errors->first('Street') }}
+            </div>
+          @else
+            <div class="invalid-feedback">
+              El campo calle es obligatorio.
+            </div>
+          @endif
+        </div>                      
+      </div>
+
+      <div class="form-group row">
+        <label for="Suburb" class="col-sm-3 col-form-label text-right">Colonia</label>
+        <div class="col-sm-9">
+          <input type="Suburb" class="form-control" name="Suburb" id="Suburb" value="{{ $shipping !== null ? $shipping->Suburb : old('Suburb') }}" maxlength="50" required>
+
+          @if ($errors->has('Suburb'))
+            <div class="invalid-validation">
+              {{ $errors->first('Suburb') }}
+            </div>
+          @else
+            <div class="invalid-feedback">
+              El campo colonia es obligatorio.
+            </div>
+          @endif
+        </div>                      
+      </div>
+
+      <div class="form-group row">
+        <label for="ZipCode" class="col-sm-3 col-form-label text-right">Código postal</label>
+        <div class="col-sm-9">
+          <input type="text" class="form-control" name="ZipCode" id="ZipCode" value="{{ $shipping !== null ? $shipping->ZipCode : old('ZipCode') }}" maxlength="5" required>
+
+          @if ($errors->has('ZipCode'))
+            <div class="invalid-validation">
+              {{ $errors->first('ZipCode') }}
+            </div>
+          @else
+            <div class="invalid-feedback">
+              El campo código postal es obligatorio.
+            </div>
+          @endif
+        </div>                      
+      </div>
+
+      <div class="form-group row">
+        <label for="State" class="col-sm-3 col-form-label text-right">Estado</label>
+        <div class="col-sm-9">
+          <input type="text" class="form-control" name="State" id="State" value="{{ $shipping !== null ? $shipping->State : old('State') }}" maxlength="25" required>
+
+          @if ($errors->has('State'))
+            <div class="invalid-validation">
+              {{ $errors->first('State') }}
+            </div>
+          @else
+            <div class="invalid-feedback">
+              El campo estado es obligatorio.
+            </div>
+          @endif
+        </div>                      
+      </div>
+      
+      <div class="form-group row">
+        <label for="City" class="col-sm-3 col-form-label text-right">Ciudad</label>
+        <div class="col-sm-9">
+          <input type="text" class="form-control" name="City" id="City" value="{{ $shipping !== null ? $shipping->City : old('City') }}" maxlength="25" required>
+
+          @if ($errors->has('City'))
+            <div class="invalid-validation">
+              {{ $errors->first('City') }}
+            </div>
+          @else
+            <div class="invalid-feedback">
+              El campo ciudad es obligatorio.
+            </div>
+          @endif
+        </div>                      
+      </div>
+
+       <div class="form-group row">
+        <label for="PhoneContact" class="col-sm-3 col-form-label text-right">Teléfono</label>
+        <div class="col-sm-9">
+          <input type="tel" class="form-control" name="PhoneContact" id="PhoneContact" value="{{ $shipping !== null ? $shipping->PhoneContact : old('PhoneContact') }}" maxlength="10" required>
+
+          @if ($errors->has('PhoneContact'))
+            <div class="invalid-validation">
+              {{ $errors->first('PhoneContact') }}
+            </div>
+          @else
+            <div class="invalid-feedback">
+              El campo teléfono es obligatorio.
+            </div>
+          @endif
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label for="References" class="col-sm-3 col-form-label text-right">Referencias</label>
+        <div class="col-sm-9">
+          <input type="text" class="form-control" name="References" id="References" value="{{ $shipping !== null ? $shipping->References : old('References') }}" maxlength="100" required>
+
+          @if ($errors->has('References'))
+            <div class="invalid-validation">
+              {{ $errors->first('References') }}
+            </div>
+          @else
+            <div class="invalid-feedback">
+              El campo referencias es obligatorio.
+            </div>
+          @endif
+
+        </div>
+      </div>
+
+      <div class="w-auto float-right">
+        <button class="btn btn-fr">Guardar</button>
+      </div>
+    </form>
+  </div>
+</div>
