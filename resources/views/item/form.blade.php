a7a134e14c642d284e4d1fa120e7ccdcdd186592
Xochitl Escamilla <xochissea@gmail.com>
2019-05-07 15:31:48 -0500

    Quitar capture option en input file

--- resources/views/item/form.blade.php
+++ resources/views/item/form.blade.php
@@ -48,7 +48,7 @@
   <div class="row js-items-container mt-2 text-center">
 
     <div class="col-sm-4 mb-5 thumb-size js-item-picture">
-      <input type="file" name="cover_item_file" id="cover_item_file" class="no-file js-item-file custom-file-input" data-type="Portada" data-name="cover" accept="image/*" capture="camera" required>
+      <input type="file" name="cover_item_file" id="cover_item_file" class="no-file js-item-file custom-file-input" data-type="Portada" data-name="cover" accept="image/*"  required>
       <label for="cover_item_file" class="card card--file-item custom-file-label">
         <span><i class="far fa-image"></i> <br>Portada</span>
       </label>
@@ -67,7 +67,7 @@
     </div>
 
     <div class="col-sm-4 mb-5 thumb-size js-item-picture">
-      <input type="file" name="front_item_file" id="front_item_file" class="no-file js-item-file custom-file-input" data-type="Foto frontal" data-name="front" accept="image/*" capture="camera" required>
+      <input type="file" name="front_item_file" id="front_item_file" class="no-file js-item-file custom-file-input" data-type="Foto frontal" data-name="front" accept="image/*" required>
       <label for="front_item_file" class="card card--file-item custom-file-label">
         <span><i class="far fa-image"></i> <br>Foto frontal</span>
       </label>
@@ -86,7 +86,7 @@
     </div>
 
     <div class="col-sm-4 mb-5 thumb-size js-item-picture">
-      <input type="file" name="label_item_file" id="label_item_file" class="no-file js-item-file custom-file-input" data-type="Foto de la etiqueta" data-name="label" accept="image/*" capture="camera" required>
+      <input type="file" name="label_item_file" id="label_item_file" class="no-file js-item-file custom-file-input" data-type="Foto de la etiqueta" data-name="label" accept="image/*" required>
       <label for="label_item_file" class="card card--file-item custom-file-label">
         <span><i class="far fa-image"></i> <br>Foto de la etiqueta</span>
       </label>
@@ -105,7 +105,7 @@
     </div>
 
     <div class="col-sm-4 mb-5 thumb-size js-item-picture">
-      <input type="file" name="back_item_file" id="back_item_file" class="no-file js-item-file custom-file-input" data-type="Foto de espaldas" data-name="back" accept="image/*" capture="camera" required>
+      <input type="file" name="back_item_file" id="back_item_file" class="no-file js-item-file custom-file-input" data-type="Foto de espaldas" data-name="back" accept="image/*" required>
       <label for="back_item_file" class="card card--file-item custom-file-label">
         <span><i class="far fa-image"></i> <br>Foto de espaldas</span>
       </label>
@@ -124,7 +124,7 @@
     </div>
 
     <div class="col-sm-4 mb-5 thumb-size js-item-picture">
-      <input type="file" accept="image/*" capture="camera" name="selfie_item_file" id="selfie_item_file" class="no-file js-item-file custom-file-input" data-type="Selfie" data-name="selfie">
+      <input type="file" accept="image/*" name="selfie_item_file" id="selfie_item_file" class="no-file js-item-file custom-file-input" data-type="Selfie" data-name="selfie">
       <label for="selfie_item_file" class="card card--file-item custom-file-label">
         <span><i class="far fa-image"></i> <br>Selfie</span>
       </label>
@@ -139,7 +139,7 @@
     </div>
 
     <div class="col-sm-4 mb-5 thumb-size js-item-picture">
-      <input type="file" name="in_item_file" id="in_item_file" class="no-file js-item-file custom-file-input" data-type="Prenda puesta" data-name="in" accept="image/*" capture="camera">
+      <input type="file" name="in_item_file" id="in_item_file" class="no-file js-item-file custom-file-input" data-type="Prenda puesta" data-name="in" accept="image/*">
       <label for="in_item_file" class="card card--file-item custom-file-label">
         <span><i class="far fa-image"></i> <br>Prenda puesta</span>
       </label>
