3f9a429ec6ec60d84b11453b5aff4896314d7d4a
Xochitl Escamilla <xochissea@gmail.com>
2019-05-23 17:51:37 -0500

    Actualizar version de css

--- resources/views/dashboard/master.blade.php
+++ resources/views/dashboard/master.blade.php
@@ -17,7 +17,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.1/css/all.css" />
 
     <!-- <link rel="stylesheet" href="css/bootstrap.min.css" /> -->
-    <link rel="stylesheet" href="{{ url('css/index.css?1.13') }}" />
+    <link rel="stylesheet" href="{{ url('css/index.css?1.14') }}" />
     <link rel="stylesheet" href="{{ url('css/fonts.css') }}" />
 
     <link rel="shortcut icon" href="{{ url('img/favicon.jpg') }}">
