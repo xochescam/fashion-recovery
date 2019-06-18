8cf2769089c431900e3b3e274c1c204810b1ddfd
Xochitl Escamilla <xochissea@gmail.com>
2019-06-18 15:29:30 -0500

    Ajustar el logo de FR

--- resources/views/dashboard/header.blade.php
+++ resources/views/dashboard/header.blade.php
@@ -1,15 +1,15 @@
 <nav id="header" class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
+    <a class="navbar-brand" href="{{ url('/') }}">
+          <img src="{{ url('img/header/transparent_logo.png') }}" alt="Fashion Recovery"/>
+      </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-      <a class="navbar-brand" href="{{ url('/') }}">
-          <img src="{{ url('img/header/transparent_logo.png') }}" alt="Fashion Recovery"/>
-      </a>
-
+    
       @if(Auth::User()->ProfileID == 4)
         <ul class="navbar-nav">
           <li class="nav-item dropdown">
