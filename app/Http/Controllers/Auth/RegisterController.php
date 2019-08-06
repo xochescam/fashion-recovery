7d39a74911a6b45aa591c3d45aa5775f14edbbcf
Xochitl Escamilla <xochissea@gmail.com>
2019-08-06 17:52:10 -0500

    Test registro sin enviar correo

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -98,8 +98,8 @@ class RegisterController extends Controller
                         ->where('email',$request->email)
                         ->first();
 
-            Mail::to($user->email)
-                 ->send(new ConfirmAccount($user, $beSeller));
+            // Mail::to($user->email)
+            //      ->send(new ConfirmAccount($user, $beSeller));
 
             DB::commit();
 
