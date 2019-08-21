9c40a899f2cf72b5db25c1b4ab77614cf05c0562
Xochitl Escamilla <xochissea@gmail.com>
2019-08-21 17:58:06 -0500

    Cambiar de orden prendas y guardarropas

--- resources/views/dashboard/ownClosets.blade.php
+++ resources/views/dashboard/ownClosets.blade.php
@@ -13,22 +13,22 @@
 					<div class="row">
 						<div class="col-sm-6 mb-5">
 						    <div class="card card--public card--item">
-                                <img class="card-img-top" src="{{ url('img/cards/closets.jpg') }}" alt="closets-img" />
-						        <div class="card-body">
-    						        <h5 class="card-title">Colecciones</h5>
-						            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-						            <a href="{{ url('closets') }}" class="btn btn-fr">Ver colecciones</a>
-						        </div>
-						    </div>
-						</div>
-						<div class="col-sm-6 mb-5">
-						    <div class="card card--public card--item">
                                 <img class="card-img-top" src="{{ url('img/cards/prendas.jpg') }}" alt="prendas-img" />
                                 <div class="card-body">
                                     <h5 class="card-title">Prendas</h5>
                                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                                     <a href="{{ url('items') }}" class="btn btn-fr">Ver prendas</a>
                                 </div>
+						    </div>
+						</div>
+						<div class="col-sm-6 mb-5">
+						    <div class="card card--public card--item">
+                                <img class="card-img-top" src="{{ url('img/cards/closets.jpg') }}" alt="closets-img" />
+						        <div class="card-body">
+    						        <h5 class="card-title">Colecciones</h5>
+						            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+						            <a href="{{ url('closets') }}" class="btn btn-fr">Ver colecciones</a>
+						        </div>
 						    </div>
 						</div>
 					</div>
