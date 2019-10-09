b37705d77aa799755569d7ae27ec62dc409c5552
Xochitl <xochitl.escamilla@ujed.mx>
2019-10-08 19:36:35 -0500

    Agregar método de pago

--- resources/views/payment/index.blade.php
+++ resources/views/payment/index.blade.php
@@ -13,9 +13,49 @@
 				@include('alerts.warning')
 			</div>
 
-			<div class="row">
+			<div class="accordion col-md-8 m-auto" id="accordionExample">
+				<div class="card">
+					<div class="card-header" id="headingOne">
+					<h2 class="mb-0">
+						<button class="btn green-link btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-controls="collapseOne">
+						<i class="fab fa-cc-visa"></i>
+						Pago con tarjetas de Crédito y Débito
+						</button>
+					</h2>
+					</div>
+
+					<div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+						<div class="card-body">
+							@include('payment.card')				
+						</div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-header" id="headingTwo">
+					<h2 class="mb-0">
+						<button class="btn green-link btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+						<i class="fab fa-cc-paypal"></i>
+						PayPal	
+						</button>
+					</h2>
+					</div>
+					<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+					<div class="card-body text-center">
+						<p class="mb-3">Para pagar con Paypal te dirigiremos a su sitio.</p>
+						
+						<a href="https://www.paypal.com/mx/signin" class="btn btn-fr btn-block w-25 m-auto">
+							<span class="spinner-border spinner-border-sm hidden" role="status" aria-hidden="true"></span>
+							Ir
+						</a>
+
+					</div>
+					</div>
+				</div>
+
+
+<!-- 			<div class="row">
 				<a href="{{ url('summary',$address->ShippingAddID) }}" class="btn btn-fr m-auto">Continuar</a>
-			</div>
+			</div> -->
       </div>
     </main>
 
