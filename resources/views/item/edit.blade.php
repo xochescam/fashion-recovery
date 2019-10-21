8c8a8f19dc75855a76635e0dc6b36e16f6ca1629
Xochitl Escamilla <xochissea@gmail.com>
2019-10-21 18:44:58 -0500

    Agregar boton de visualizar y eliminar prenda

--- resources/views/item/edit.blade.php
+++ resources/views/item/edit.blade.php
@@ -9,8 +9,16 @@
 
           <p class="text-center mb-5 w-100">Cambio ipsum dolor sit amet, consectetur adipisicing elit. Suscipit qui ad, commodi nostrum repudiandae ipsam soluta excepturi.</p>
 
+          <div class="col-md-8 offset-md-2 mb-4">
+              <a href="{{ url('items/'.$item->ItemID.'/public') }}" target="_blank" class="btn btn-fr mb-4 float-right">
+                Visualizar prenda 
+              </a>
+            </div>
+
           <div class="col-md-8 offset-md-2">
             <form method="POST" action="{{ route('item.update',$item->ItemID) }}" class="needs-validation" enctype="multipart/form-data" novalidate>
+
+            
 
               <div id="app">
               @include('item.form')
@@ -18,7 +26,7 @@
 
               <div class="text-center mt-5 row">
                 <div class="col-md-6">
-                  <a href="{{ url('item/'.$item->ItemID.'/full-delete') }}" class="btn btn-secondary w-100">
+                  <a href="{{ url('item/'.$item->ItemID.'/full-delete') }}" class="btn btn-danger w-100">
                     <span class="spinner-border spinner-border-sm hidden" role="status" aria-hidden="true"></span>
                     Eliminar prenda
                   </a>
