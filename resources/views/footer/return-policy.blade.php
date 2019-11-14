a4ca6c89adb00b76912333e4d6bdafcbe3b643ae
Xochitl <xochitl.escamilla@ujed.mx>
2019-11-14 15:46:59 -0600

    Politicas de devolución

--- resources/views/footer/return-policy.blade.php
+++ resources/views/footer/return-policy.blade.php
@@ -6,10 +6,49 @@
     <div class="container py-5">
       <div class="row">
         <div class="col-12">
-          <h1 class="text-left TituloFR my-4">Política de Devolución</h1>
+          <h1 class="text-left TituloFR my-4">Política de Devoluciones</h1>
         </div>
         <div class="col-12 text-justify mx-2">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde dolor nisi suscipit possimus voluptates ea aspernatur voluptatum accusamus eveniet? Ipsa architecto reprehenderit et eveniet voluptas laboriosam numquam quia, sequi consectetur.</p>
+          <p>Dependiendo de la razón por la cual deseas devolver una prenda, en FASHION RECOVERY
+          podemos hacerte un reembolso. Ten en cuenta que por cuestiones de higiene personal que
+          pudieren poner en peligro la salud de nuestros usuarios, no hay cambios ni devoluciones en trajes
+          de baño ni en ropa interior.</p>
+
+          <p>Es muy importante que tengas en consideración que dispones de 24 horas a partir de la recepción
+          de tu pedido para notificar a FASHION RECOVERY sobre el deseo de devolución. El procedimiento
+          para solicitar una devolución es el siguiente:</p>
+
+          <ol>
+            <li>Debes iniciar sesión en nuestra página www.fashionrecovery.com.mx</li>
+            <li>Dirígete a MI CUENTA, después a MIS PEDIDOS y enseguida a PEDIDOS EN CURSO, en
+            donde podrás ver el historial de tus compras que se encuentran en curso o que han sido
+            entregadas y se encuentran dentro del periodo válido para solicitar devolución. Si no
+            visualizas la prenda que deseas devolver en PEDIDOS EN CURSO, es debido a que el
+            periodo de devolución ha finalizado, por lo que podrás visualizar la misma en PEDIDOS
+            FINALIZADOS.</li>
+            <li>Selecciona la prenda que deseas devolver y presiona el botón DEVOLUCIÓN,
+            seleccionando la razón por la cual deseas realizar la devolución. Te sugerimos adjuntar
+            imágenes de la prenda para sustentar el motivo de la devolución, con lo cual el proceso de
+            devolución será más rápido.</li>
+            <li>El equipo de FASHION RECOVERY analizará tu solicitud, y en caso de pre-aprobar la
+            devolución, recibirás un correo electrónico con un número de guía la cual deberás
+            descargar e imprimir.</li>
+            <li>Prepara el paquete para realizar la devolución y deposita la prenda en cuestión dentro del
+            mismo. Asegúrate de que el paquete no exceda las dimensiones adecuadas según las
+            características de la prenda para evitar el rechazo del paquete. FASHION RECOVERY no es
+            responsable por el rechazo de paquetes, siendo única y exclusivamente decisión de la
+            empresa de paquetería en cuestión, lo cual se apega a sus propios términos y condiciones.</li>
+            <li>Contarás con 3 días hábiles para depositar el paquete con la prenda a devolver en la
+            empresa de paquetería que corresponda.</li>
+            <li>Una vez que el vendedor reciba la prenda, el equipo de FASHION RECOVERY analizará el
+            estado de la prenda para determinar que su condición coincida con el motivo de la
+            devolución.</li>
+            <li>Si la prenda cumple con los lineamientos de devolución, FASHION RECOVERY te generará
+            un reembolso, para el cual podrás seleccionar la acreditación inmediata en MI CARTERA, o
+            bien, solicitar un retiro a la cuenta bancaria de tu elección ingresando la CLABE
+            Interbancaria. Los retiros a cuentas bancarias pueden tardar hasta 5 días hábiles, lo cual
+            depende de cada institución bancaria y es ajeno a FASHION RECOVERY.</li>
+          </ol>
         </div>
   </main>
 
