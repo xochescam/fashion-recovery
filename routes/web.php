db93192bbf0089a52b020e1ee92de1a7863f56b3
Xochitl <xochitl.escamilla@ujed.mx>
2019-11-14 19:16:02 -0600

    Cambiar ruta del guardarropa

--- routes/web.php
+++ routes/web.php
@@ -136,7 +136,7 @@ Route::group(['middleware' => ['auth']], function () {
 	Route::post('update-selfie/{userId}', 'SellerController@updateSelfie');
 	Route::get('welcome/seller', 'SellerController@sellerWelcome');
 
-	Route::get('auth/update/guardarropa/{IsPaused}', 'SellerController@guardarropaStatus');
+	Route::get('update/{Type}/{IsPaused}', 'SellerController@guardarropaStatus');
 
 	//Item
 	Route::get('items', 'ItemController@index');
