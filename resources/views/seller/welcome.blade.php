4b83ad184c19f294a797f0f76cd4a878d6dcf1d6
Xochitl Escamilla <xochissea@gmail.com>
2020-02-25 18:41:42 -0600

    Bienvenida al vendedor

--- resources/views/seller/welcome.blade.php
+++ resources/views/seller/welcome.blade.php
@@ -6,7 +6,83 @@
       <div class="container py-5">
         <div class="row">
           <div class="col-md-10 offset-md-1">
-            <h2 class="text-center TituloFR my-4 mb-5">Bienvenida al vendedor</h2>
+            <h2 class="text-center TituloFR my-4 mb-5">¡Únete al movimiento!</h2>
+
+            <p class="mb-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis nemo eaque nihil odio? Molestiae eligendi omnis laudantium, est quis blanditiis aliquid tempora. Nam autem a facilis non tempore velit! Hic?</p>
+            
+            <h5 class="mb-4 TituloFR">Cuida al planeta vendiendo tu Guardarropa</h5>
+
+            <div class="row">
+              <div class="col-sm-4 mb-4">
+                <div class="card">
+                  <img src="..." class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">1. Agrega tus prendas</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-4 mb-4">
+                <div class="card">
+                  <img src="..." class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">2. Envialo con amor</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-4 mb-4">
+                <div class="card">
+                  <img src="..." class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">3. ¡Haz la diferencia!</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="w-100 text-right mb-4">
+              <a href="#" class="btn btn-fr btn-sm" role="button">Vende tu Guardarropa</a>
+            </div>
+
+            <hr class="my-5">
+
+            <h5 class="mb-4 TituloFR">Súmate adquiriendo prendas únicas</h5>
+
+            <div class="row">
+              <div class="col-sm-4 mb-4">
+                <div class="card">
+                  <img src="..." class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">1. Busca tu prenda ideal</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-4 mb-4">
+                <div class="card">
+                  <img src="..." class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">2. Adquiere la prenda</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-4 mb-4">
+                <div class="card">
+                  <img src="..." class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">3. ¡Dale un segundo uso!</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="w-100 text-right">
+              <a href="#" class="btn btn-fr btn-sm" role="button">Buscar prendas</a>
+            </div>
 
 
           </div>
