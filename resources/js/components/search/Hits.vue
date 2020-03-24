c686a9e82a1d702cbac394d483721b7b2a22d207
Xochitl Escamilla <xochissea@gmail.com>
2020-03-24 17:24:27 -0600

    Agregar storage

--- resources/js/components/search/Hits.vue
+++ resources/js/components/search/Hits.vue
@@ -17,7 +17,7 @@
                         slot-scope="{ item }">
                         <div class="card card--public card--item shadow p-3 bg-white rounded d-flex align-items-stretch h-100">
                                 
-                            <img class="card-img-top" :src="path+'/'+item.ThumbPath" alt="" height="200px;">
+                            <img class="card-img-top" :src="path+'/storage/'+item.ThumbPath" alt="" height="200px;">
                             <div class="card-body px-0 p-lg-3">
                                     
                                 <h4 class="card-title mb-0">
