074031d44bc2173d088e7aca6856cb50856a696d
Xochitl Escamilla <xochissea@gmail.com>
2020-04-02 19:28:34 -0600

    Agregar js de componentes

--- resources/views/search/search.blade.php
+++ resources/views/search/search.blade.php
@@ -55,7 +55,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-    <script type="text/javascript" src="{{ url('js/app.js?1.13') }} "></script>
+    <script type="text/javascript" src="{{ url('js/public-components.js?1.13') }} "></script>
 
     <script type="text/javascript" src="{{ url('js/public.js?1.0') }} "></script>
     
