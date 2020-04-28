33b20637bdf77745ae4fabaf7718d84a18446c02
Xochitl Escamilla <xochissea@gmail.com>
2020-04-28 18:51:32 -0500

    Actualizar url

--- resources/views/emails/reset-password.blade.php
+++ resources/views/emails/reset-password.blade.php
@@ -12,7 +12,7 @@
                 Para hacerlo da clic en el siguiente enlace para restablecer tu contraseña:
             </p>
 
-            <a href="{{ url($token) }}" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; border-radius: 3px; box-shadow: 0 2px 3px rgba(0, 0, 0, 0.16); color: #FFF; display: inline-block; text-decoration: none; -webkit-text-size-adjust: none; background-color: #009c77; border-top: 10px solid #009c77; border-right: 18px solid #009c77; border-bottom: 10px solid #009c77; border-left: 18px solid #009c77;">Restablecer contraseña</a>
+            <a href="https://www.fashionrecovery.com.mx/{{$token}}" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; border-radius: 3px; box-shadow: 0 2px 3px rgba(0, 0, 0, 0.16); color: #FFF; display: inline-block; text-decoration: none; -webkit-text-size-adjust: none; background-color: #009c77; border-top: 10px solid #009c77; border-right: 18px solid #009c77; border-bottom: 10px solid #009c77; border-left: 18px solid #009c77;">Restablecer contraseña</a>
 
             <p style="color:#666; font:300 14px sans-serif; line-height:1.5em; margin: 1em 0; padding: 20px 40px 50px;">
 				Este enlace sólo es válido en los siguientes 60 minutos desde que recibes este mensaje. En caso de que expire, puedes solicitarlo nuevamente.<br>
