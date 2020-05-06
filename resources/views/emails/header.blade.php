f21e39998f8fdcd8d698dcc4d3cdccc83c99a337
Xochitl Escamilla <xochissea@gmail.com>
2020-05-06 17:54:13 -0500

    Agregar items al email

--- resources/views/emails/header.blade.php
+++ resources/views/emails/header.blade.php
@@ -1,4 +1,4 @@
-<tr style="display: block;height: 120px;margin: 0 auto;background-image: url(http://fashionrecovery.azurewebsites.net/img/header/BoardGreen.jpg);background-color: #009c77;">
+<tr style="display: flex;align-items:center;justify-content: center;height: 120px;margin: 0 auto;background-image: url(http://fashionrecovery.azurewebsites.net/img/header/BoardGreen.jpg);background-color: #009c77;">
 	<td style="padding: 30px 150px;">
 		<img src="{{ url('http://fashionrecovery.azurewebsites.net/img/header/transparent_logo.png') }}" alt="Fashion Recovery" style="width: 400px;"/>
 	</td>
--- resources/views/emails/sold.blade.php
+++ resources/views/emails/sold.blade.php
@@ -7,7 +7,7 @@
 	            <tr>
 	                <td width="700" valign="top">
 
-	                	<p style="color: #444; font: 400 20px sans-serif; line-height: 1.6em; margin-top: 1.5em;padding: 0; text-align: left; margin-left: 50px;">
+	                	<p style="color: #444; font: 400 20px sans-serif; line-height: 1.6em; margin-top: 1.5em;padding: 0; text-align: center;">
                             ¡Felicidades, tu compra ha sido procesada!
 	                    </p>
 
@@ -26,7 +26,35 @@
                             <b>Total del pedido:</b> ${{ $Total }}
 	                    </p>
 
-	                    <div style="margin:50px;text-align:left;margin-top: 0px;display: inline-flex;">
+						<ul style="font: 200 14px sans-serif;border-top: 1px solid rgba(0,0,0,.125); margin-left: 50px; margin-right: 50px; padding: 0px;  width: 700px; margin-top: 20px;">
+
+			          		@foreach($items as $item)
+
+			          			<li style="display:block; border-bottom: 1px solid rgba(0,0,0,.125);">
+							  		<div style="display: flex;">
+									    <div style="width: 30%;">
+									      <img src="{{ url('storage/'.$item->ThumbPath) }}" alt="">
+									    </div>
+									    <div style="width: 70%; padding-left: 20px;">
+									      <div>
+									      	<div>
+												<p>{{ $item->ItemDescription }}</p>
+									      		<p>{{ $item->ActualPrice }}</p>
+									      	</div>
+									      	<p>
+									      		<small>Talla: {{ $item->SizeID }}</small> <br>
+												<small>Marca: {{ $item->BrandName }}</small> <br>
+												<small>Vendedor: {{ $item->Alias }}</small>
+									      	</p>
+									      </div>
+									    </div>
+									</div>
+						  		</li>
+
+			          		@endforeach
+						</ul>
+
+	                    <div style="text-align:center;margin:auto;display: block;width:700px; margin-bottom: 50px; margin-top: 50px;">
 	                        <a href="{{ url('orders') }}" target="_blank" style="background-color: #009c77;
     color: #fff;display: inline-block;font: 400 14px/1 &quot;Open Sans&quot;, sans-serif;font-size: 0.875rem;padding: 1.2em 1.3em; transition: background-color 200ms ease-out;text-decoration: none;cursor: pointer;">
     							Ver pedido 
