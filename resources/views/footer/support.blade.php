dfd78bd68f597403a1e629f39b3bae1524794428
Xochitl Escamilla <xochissea@gmail.com>
2020-05-22 17:11:41 -0500

    Cambiar titulo

--- resources/views/footer/support.blade.php
+++ resources/views/footer/support.blade.php
@@ -6,7 +6,7 @@
     <div class="container py-5">
       <div class="row">
         <div class="col-12">
-          <h1 class="text-left TituloFR my-4">Ayuda</h1>
+          <h1 class="text-left TituloFR my-4">¿Cómo funciona Fashion Recovery?</h1>
         </div>
         <div class="col-12 text-justify mx-2">
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde dolor nisi suscipit possimus voluptates ea aspernatur voluptatum accusamus eveniet? Ipsa architecto reprehenderit et eveniet voluptas laboriosam numquam quia, sequi consectetur.</p>
