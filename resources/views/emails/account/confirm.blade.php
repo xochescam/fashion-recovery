b9a5b221c56f6e0ac8d2797b668c7b4a84664f5c
Xochitl Escamilla <xochissea@gmail.com>
2020-05-23 15:29:52 -0500

    Ajustar el correo

--- resources/views/emails/account/confirm.blade.php
+++ resources/views/emails/account/confirm.blade.php
@@ -5,28 +5,34 @@
 	    <td width="700" valign="top">
 	        <table cellpadding="0" cellspacing="0" border="0" align="center">
 	            <tr>
-	                <td width="700" valign="top">
-
-	                	<p style="color: #444; font: 400 20px sans-serif; line-height: 1.6em; margin-top: 1.5em;padding: 0; text-align: left; margin-left: 50px;">
-	                        Confirmación de cuenta
-	                    </p>
+	                <td style="padding-top: 40px;padding-bottom: 40px;" width="700px" valign="top">
 
 	                    <p style="color: #444; font: 400 15px sans-serif; line-height: 1.6em;padding: 0; text-align: left;margin-left: 50px;">
-	                        Hola {{ $name }}, bienvenido(a) a Fashion Recovery
+							¡Hola <b>{{ $name }}</b>, te damos la bienvenida a FASHION RECOVERY!
 	                    </p>
 
-	                    <p style="color: #444;font: 300 15px sans-serif;margin-left: 50px;text-align: left;;    margin-bottom: 30px; margin-top: 20px;">
-	                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis excepturi laudantium dolores vitae delectus. Deserunt itaque quia at recusandae, soluta, impedit hic corrupti architecto, qui esse eveniet deleniti nam quaerat.
+	                    <p style="color: #444;font: 300 15px sans-serif;margin-left: 50px;margin-right: 50px;text-align: left; margin-bottom: 30px; margin-top: 20px;">
+						Tu cuenta ha sido activada exitosamente, ahora solo nos falta confirmar que esta es tu dirección de correo electrónico.
 	                    </p>
 
-	                    <div style="margin:50px;text-align:left;margin-top: 0px;display: inline-flex;">
-	                        <a href="{{ url('confirm-account/'.$UserID.'/'.$beSeller) }}" target="_blank" style="background-color: #009c77;
+	                    <div style="margin-right:50px;margin-left:50px;width: 700px;margin-top: 0px;display: inline-flex;margin-bottom: 10px;">
+	                        <a href="{{ url('confirm-account/'.$UserID.'/'.$beSeller) }}" target="_blank" style="background-color: #009c77;margin: auto;
     color: #fff;display: inline-block;font: 400 14px/1 &quot;Open Sans&quot;, sans-serif;font-size: 0.875rem;padding: 1.2em 1.3em; transition: background-color 200ms ease-out;text-decoration: none;cursor: pointer;">
 								Confirmar mi cuenta
 							</a>
-
-							<p style="color: #444;font: 500 15px sans-serif; margin-left: 40px;">No fui yo, reportar este email</p>
-		                </div>
+						</div>
+						
+						<p style="color: #444;font: 300 15px sans-serif;margin-left: 50px;margin-right: 50px;text-align: left; margin-bottom: 30px; margin-top: 20px;">
+							Atentamente, El equipo de FASHION RECOVERY
+						</p>
+						
+						<p style="color: #444;font: 300 15px sans-serif;margin-left: 50px;margin-right: 50px;text-align: left; margin-bottom: 30px; margin-top: 20px;">
+							<b>Nota:</b> No respondas a este mensaje de correo electrónico. Se ha
+							enviado desde una dirección exclusiva para notificaciones que no
+							acepta mensajes entrantes. Si no te has registrado en FASHION
+							RECOVERY y crees que tu correo electrónico ha sido utilizado por
+							alguien más, <a href="#" style="text-decoration: none;color: #444"> <b>puedes eliminar la cuenta</b> </a>
+	                    </p>
 	                </td>
 	            </tr>
 	        </table>
---
+++ resources/views/emails/footer.blade.php
@@ -0,0 +1,19 @@
+<tr style="width: 700px;background-color: #fff;height: auto;text-align: center;">
+	<td>
+        <div style="margin-top: 30px;text-align: left;margin-left: 50px;margin-right: 50px;height: 50px;display: flex;align-items: center;justify-content: space-between;">
+            <a href="{{ url('support') }}" style="color: #969696; font: 400 14px sans-serif;text-decoration: none;">Ayuda</a>
+            <a href="{{ url('terms') }}" style="color: #969696; font: 400 14px sans-serif;text-decoration: none;">Términos y condiciones</a>
+            <a href="{{ url('privacy') }}" style="color: #969696; font: 400 14px sans-serif;text-decoration: none;">Aviso de privacidad</a>
+            <a href="{{ url('about') }}" style="color: #969696; font: 400 14px sans-serif;text-decoration: none;">Sobre FASHION RECOVERY</a> 
+        </div>
+
+        <div style="height: 50px;display: flex;align-items: center;justify-content: space-around;margin-left: 300px;margin-right: 300px;">
+            <a href="#">FB</a>
+            <a href="#">T</a>
+            <a href="#">IG</a>
+        </div>
+
+        <p style="font: 400 14px sans-serif;color: #969696;margin-top: 30px;">©FASHION RECOVERY, todos los derechos reservados.</p>
+       
+    </td>
+</tr>
