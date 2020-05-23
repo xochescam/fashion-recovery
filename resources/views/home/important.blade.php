bdb5f7668f6094c350541311f73eef91cb414207
Xochitl Escamilla <xochissea@gmail.com>
2020-05-23 14:17:31 -0500

    Ajustar vista de guardarropas destacados

--- resources/views/home/important.blade.php
+++ resources/views/home/important.blade.php
@@ -1,57 +1,43 @@
 <section id="LosDestacados" >
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col text-center text-uppercase mb-4">
-          <small>Echa un vistazo a nuestros</small>
-          <h2>Closets Destacados</h2>
-        </div>
-      </div>
-      <div class="row justify-content-around shadow-lg p-3 mb-5 bg-light rounded mx-md-5">
-
-        <div class="col-lg-3 col-md-4 col-sm-6 mb-4 d-flex">
-          <div class="card card--public shadow p-3 mb-5 bg-white rounded">
-            <img class="card-img-top img-thumbnail" src="{{ url('img/important/women.jpg') }}" alt="Closet para Mujer">
-            <div class="card-body">
-              <h5 class="card-title">Mujeres</h5>
-              <p class="card-text">Encuentra aquí los mejores estilos y los look más chic & hot del momento.</p>
-              <a href="{{ url('search/Mujeres') }}" class="btn btn-fr">Ver más</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-md-4 col-sm-6 mb-4 d-flex">
-          <div class="card card--public shadow p-3 mb-5 bg-white rounded">
-            <img class="card-img-top img-thumbnail" src="{{ url('img/important/men.jpg') }}" alt="Closet para Hombre">
-            <div class="card-body">
-              <h5 class="card-title">Hombres</h5>
-              <p class="card-text">¿Casual? ¿Negocios? ¿Relajado? ¿Deportivo?  ¡Tú elijes! Arma tu look aquí.</p>
-              <a href="{{ url('search/Hombres') }}" class="btn btn-fr">Ver más</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-md-4 col-sm-6 mb-4 d-flex">
-          <div class="card card--public shadow p-3 mb-5 bg-white rounded">
-            <img class="card-img-top img-thumbnail" src="{{ url('img/important/kid.jpg') }}" alt="Closet para Niños">
-            <div class="card-body">
-              <h5 class="card-title">Niños</h5>
-              <p class="card-text">Vístelos ideal para cada ocasión, encuentras las mejores prendas y accesosios para los pequeñitos del hogar.</p>
-              <a href="{{ url('search/Niños') }}" class="btn btn-fr">Ver más</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-md-4 col-sm-6 mb-4 d-flex">
-          <div class="card card--public shadow p-3 mb-5 bg-white rounded">
-            <img class="card-img-top img-thumbnail" src="{{ url('img/important/maternity.jpg') }}" alt="Closet para Bebés y Maternidad">
-            <div class="card-body">
-              <h5 class="card-title">Bebés y Maternidad</h5>
-              <p class="card-text">Prepara lo necesario para el gran momento, encuentra los mejores conjuntos para ti y para tu bebé.</p>
-              <a href="{{ url('search/Maternidad') }}" class="btn btn-fr">Ver más</a>
-            </div>
-          </div>
-        </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col text-center text-uppercase mb-4">
+        <small>Echa un vistazo a nuestros</small>
+        <h2>Closets Destacados</h2>
       </div>
     </div>
-  </section>
+
+    <div class="row shadow-lg p-3 mb-5 bg-light rounded mx-md-5">
+
+        <div class="col-lg-3 col-md-4 col-sm-6 banner-important">
+          <a href="{{ url('search/Mujeres') }}">
+            <img class="card-img-top" src="{{ url('img/important/women.jpg') }}" alt="Closet para Mujer">
+            <h5 class="banner-important__title">Mujeres</h5>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-4 col-sm-6 banner-important">
+          <a href="{{ url('search/Hombres') }}">
+            <img class="card-img-top" src="{{ url('img/important/men.jpg') }}" alt="Closet para Hombres">
+            <h5 class="banner-important__title">Hombres</h5>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-4 col-sm-6 banner-important">
+          <a href="{{ url('search/Niños') }}">
+            <img class="card-img-top" src="{{ url('img/important/kid.jpg') }}" alt="Closet para Niños">
+            <h5 class="banner-important__title">Niños</h5>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-4 col-sm-6 banner-important mr-0">
+          <a href="{{ url('search/Maternidad') }}">
+            <img class="card-img-top" src="{{ url('img/important/maternity.jpg') }}" alt="Closet para Bebés y Maternidad">
+            <h5 class="banner-important__title">Bebés y Maternidad</h5>
+          </a>
+        </div>
+
+    </div>
+  </div>
+</section>
 
