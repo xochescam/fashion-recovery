8578fb8edcbb79a56748205fc3021e523acb4eee
Xochitl Escamilla <xochissea@gmail.com>
2020-05-28 16:14:05 -0500

    Habilitar opciones de bloquear/desbloquear

--- resources/views/users/partials/sellers.blade.php
+++ resources/views/users/partials/sellers.blade.php
@@ -4,8 +4,8 @@
             <th scope="col">Alias</th>
             <th scope="col">Nombre</th>
             <th scope="col">Miembro desde</th>
-<!--             <th scope="col"></th>
- -->        </tr>
+            <th scope="col"></th>
+        </tr>
     </thead>
     <tbody>
         @foreach($users[2] as $user)
@@ -13,7 +13,7 @@
                 <th> <a href="{{ url('user',$user->Alias) }}" class="green-link">{{ $user->Alias }}</a> </th>
                 <td> {{ $user->Name }} {{ $user->Lastname }} </td>
                 <td> {{ $user->CreationDate }} </td>
-                <!-- <td>
+                <td>
                     @if($user->IsBlocked)
                         <a class="btn btn-danger btn-sm" href="{{ url('unblock',$user->Alias) }}" role="button">
                             Desloquear
@@ -24,7 +24,7 @@
                         </a>
                     @endif
                 </td>
-            </tr> -->
+            </tr>
         @endforeach
     </tbody>
 </table>
