1543f98c461cc4e51db335e06bacee49f0302a92
Xochitl Escamilla <xochissea@gmail.com>
2020-05-29 20:46:22 -0500

    Modelo del vendedor

---
+++ app/Seller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+use DB;
+
+class Seller extends Model
+{
+    public $timestamps = false;
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'fashionrecovery.GR_033';
+    protected $primaryKey = 'SellerID';
+}
