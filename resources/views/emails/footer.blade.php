54fb9e0a1bf86070981dd3521ec5c18b90023d0e
Xochitl Escamilla <xochissea@gmail.com>
2020-06-08 17:38:42 -0500

    Ajustar center de footer

--- resources/views/emails/footer.blade.php
+++ resources/views/emails/footer.blade.php
@@ -1,13 +1,13 @@
 <tr style="width: 700px;background-color: #fff;height: auto;text-align: center;">
 	<td>
-        <div style="margin-top: 30px;text-align: left;margin-left: 50px;margin-right: 50px;height: 50px;display: flex;align-items: center;justify-content: center;">
+        <div style="margin-top:30px; margin-left:50px; margin-right:50px; height:50px;text-align: center;">
             <a href="{{ url('support') }}" style="color: #969696; font: 400 14px sans-serif;text-decoration: none;margin-right: 20px;">Ayuda</a>
             <a href="{{ url('terms') }}" style="color: #969696; font: 400 14px sans-serif;text-decoration: none;margin-right: 20px;">Términos y condiciones</a>
             <a href="{{ url('privacy') }}" style="color: #969696; font: 400 14px sans-serif;text-decoration: none;margin-right: 20px;">Aviso de privacidad</a>
             <a href="{{ url('about') }}" style="color: #969696; font: 400 14px sans-serif;text-decoration: none;margin-right: 20px;">Sobre FASHION RECOVERY</a> 
         </div>
 
-        <div style="height:50px; display:flex; align-items:center; justify-content: center;">
+        <div style="height:50px; text-align: center;">
             <a href="#">
                 <img src="https://fashionrecovery.com.mx/img/facebook.svg" style="height:20px;margin-right: 50px;">
             </a>
