22ca97c141d47f59392963d2713eb24510fc5d66
Xochitl Escamilla <xochissea@gmail.com>
2020-06-15 19:36:21 -0500

    Eliminar test del email

--- app/Mail/ErrorPackPack.php
+++ app/Mail/ErrorPackPack.php
@@ -40,7 +40,7 @@ class ErrorPackPack extends Mailable
         return $this->subject('Error en Pack & Pack')
                     ->view('emails.error-packpack')
                     ->with([
-                        'response' => 'hola',
+                        'response' => $this->response,
                         'string' => str_random(255)
                     ]);
 
