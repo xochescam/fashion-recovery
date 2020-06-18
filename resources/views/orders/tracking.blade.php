2761a3ae2bf49a6304f3c761e4e33f9ec14719d5
Xochitl Escamilla <xochissea@gmail.com>
2020-06-17 19:14:19 -0500

    Agregar estilo al tracking

--- resources/views/orders/tracking.blade.php
+++ resources/views/orders/tracking.blade.php
@@ -7,15 +7,19 @@
         	<h2 class="text-center TituloFR my-4 mb-6">¡Tu pedido esta en camino!</h2>
 
 			<div class="col-md-10 m-auto">
-				<ul class="mt-5">
+				<ul class="tracking mt-5">
 					@foreach($tracking as $track)
-						<li class="mb-5">
-							<p class="mb-0"> {{ $track['status'] }} </p>
-							
-							<small> 
-								{{ $track['location'] === '' ? $tracking[0]['location'] :  $track['location']  }} 
-								{{ isset($track['date']) ?  'el '.$track['date'] : '' }} 
-							</small>
+						<li class="tracking__place mb-5">
+							<span 
+								class="tracking__dot {{ $track['status'] === 'Destino' ? 'tracking__dot--gray' : ($tracking[count($tracking) - 2]['location'] === $track['location'] ? 'tracking__dot--penultimate' : '') }}"></span>
+							<p class="mb-0"> 
+								{{ $track['status'] }} 
+								<br>
+								<small> 
+									{{ $track['location'] === '' ? $tracking[0]['location'] :  $track['location']  }} 
+									{{ isset($track['date']) ?  'el '.$track['date'] : '' }} 
+								</small>	
+							</p>
 						</li>
 					@endforeach
 				</ul>
