792b94009a4944581c805f51af3d17986fa70e45
Xochitl Escamilla <xochissea@gmail.com>
2020-07-01 15:05:45 -0500

    Mostrar vista de devoluciones

---
+++ resources/views/orders/partials/return.blade.php
@@ -0,0 +1,18 @@
+@if(count($return) == 0)
+	<p>¿Buscas un pedido? Se han enviado todos los pedidos.</p>
+@else
+	<p>Tienes {{ count($return) }} pedido{{ count($return) > 1 ? 's' : '' }} realizado{{ count($return) > 1 ? 's.' : '.' }}</p>
+
+	<div class="card">
+  		<ul class="list-group list-group-flush w-100">
+			@foreach($return as $order)
+          		@foreach($items[$order->OrderID] as $item)
+
+				  @include('orders.partials.item')
+
+          		@endforeach
+          	@endforeach
+		</ul>
+	</div>	
+			
+@endif
