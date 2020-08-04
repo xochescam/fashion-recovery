a6fedf01c4c3579f4cd010d32d78374d4f5e8ddf
Xochitl Escamilla <xochissea@gmail.com>
2020-08-04 17:27:28 -0500

    Añadir componente del proceso de pago

--- resources/views/payment/index.blade.php
+++ resources/views/payment/index.blade.php
@@ -27,88 +27,16 @@
 			@include('alerts.warning')
 		</div>
 
-		<div class="row">
-			<div class="col-md-9 order-2 order-md-1 mb-4">
-				<div class="accordion" id="accordionExample">
-					<div class="card">
-						<div class="card-header p-0" id="headingOne">
-							<a class="btn green-link w-100 p-3 d-flex justify-content-start align-items-center" type="button" aria-expanded="true" data-toggle="collapse" data-target="#collapseOne" aria-controls="collapseOne">
-								<img src="{{ url('/img/logos/mercadopago.png') }}" class="mr-2 height-15" alt="">
-								<span>Tarjetas de Crédito y Débito</span>	
-							</a>
-						</div>
-
-						<div id="collapseOne" class="collapse show border-bottom" aria-labelledby="headingOne" data-parent="#accordionExample">
-							<div class="card-body">
-								<card-payment
-									csrf="{{ csrf_token() }}"
-									shipping="{{ $address->ShippingAddID }}"
-									:shippingcost="{{ $shippingCost }}"
-								>
-								</card-payment>			
-							</div>
-						</div>
-					</div>
-					<!--<div class="card">
-						 <div class="card-header p-0" id="headingTwo">
-							<a class="btn green-link w-100 p-3 d-flex justify-content-start align-items-center" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-controls="collapseTwo">
-								<img src="{{ url('/img/logos/paypal-logo.png') }}" class="mr-1 height-15" alt="">
-								<span>PayPal</span>	
-							</a>
-						</div>
-						<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-							<div class="card-body text-center">
-								<p class="mb-3">Para pagar con Paypal te dirigiremos a su sitio.</p>
-									
-								<a href="https://www.paypal.com/mx/signin" class="btn btn-fr btn-block w-25 m-auto">
-									<span class="spinner-border spinner-border-sm hidden" role="status" aria-hidden="true"></span>
-									Ir
-								</a>
-							</div>
-						</div> 
-					</div>-->
-				</div>					
-		</div>
-		<div class="col-md-3 order-1 order-md-2 mb-4">
-			<div class="card bg-light fit-height">
-				<div class="card-body w-full">
-					<h5 class="card-title"><b>Resumen del pedido</b></h5>
-
-					<table class="w-100 mt-4">
-						@if(isset($devTotal))
-							<tr>
-								<td>Cartera:</td>
-								<td class="text-right">${{ $devTotal }} </td>
-							</tr>
-						@endif
-						<tr>
-							<td>Subtotal:</td>
-							<td class="text-right">${{ $subtotal }} </td>
-						</tr>
-						<tr>
-							<td>Envio:</td>
-							<td class="text-right">${{ $shippingCost }}</td>
-						</tr>
-					</table>
-					<hr>
-
-					<table class="w-100 mt-4">
-						<tr>
-							<td>Total:</td>
-							<td class="text-right green-color">
-								<h5><b>${{ $total }}</b></h5>
-							</td>
-						</tr>
-					</table>
-
-					@if(isset($devTotal) && $devTotal > $subtotal)
-						<div class="alert alert-success w-100 p-2 mt-3 text-center font-13" role="alert">
-							Te quedan ${{ $devTotal - ($subtotal + $shippingCost)  }} en cartera.
-						</div>
-					@endif
-				</div>
-			</div>
-		</div>	
+		<payment-component
+			amount="{{ isset($amount) ? $amount : '0' }}"
+			:address="{{ json_encode($address) }}"
+			:shippingcost="{{ $shippingCost }}"
+			:subtotal="{{ $subtotal}}"
+			:devtotal="{{ $devTotal }}"
+			:total="{{ $total }}"
+			csrf="{{ csrf_token() }}"
+		>
+		</payment-component>
     </div>
 </main>
 
