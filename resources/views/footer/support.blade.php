fcb43c7c4893e3b4e344254dc669504ea45df414
Xochitl Escamilla <xochissea@gmail.com>
2020-09-03 15:58:40 -0500

    Actualizar html de ¿Cómo funciona?

--- resources/views/footer/support.blade.php
+++ resources/views/footer/support.blade.php
@@ -4,85 +4,187 @@
 
 	 <main id="main">
       <div class="container py-5">
-        <div class="row">
-            <h2 class="text-center TituloFR my-4 mb-5">¿Cómo funciona FASHION RECOVERY?”</h2>
+            <div class="col-md-12 info-steps p-0">
+              <div class="info-steps__header">
+                <img src="{{ url('img/header/transparent_logo.png') }}" class="info-steps__header-logo" alt="">
 
-            <p class="mb-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis nemo eaque nihil odio? Molestiae eligendi omnis laudantium, est quis blanditiis aliquid tempora. Nam autem a facilis non tempore velit! Hic?</p>
+                <p class="info-steps__header-title">¿CÓMO FUNCIONA?</p>
+                <p class="info-steps__header-subtitle">5 PASOS PARA VENDER TU CLÓSET</p>
+              </div>
+
+              <div class="info-steps__content">
+                <div class="info-steps__item ">
+                  <div class="info-steps__item-img order-1 order-md-0 mt-4 mt-md-0">
+                    <img src="{{ url('img/info/3.png') }}"  alt="">
+                  </div>
+                  
+                  <span class="info-steps__step order-0 order-md-1 ">1</span>
+                  <p class="info-steps__text order-2 order-md-2 mb-5 mb-md-0">CREA TU PERFIL EN FASHION RECOVERY</p>
+                </div>
+
+                <div class="info-steps__item">
+                <div class="info-steps__item-img order-1 order-md-0 mt-4 mt-md-0">
+                    <img src="{{ url('img/info/4.png') }}"  alt="">
+                  </div>
+                  <span class="info-steps__step order-0 order-md-1">2</span>
+                  <p class="info-steps__text order-2 order-md-2 mb-5 mb-md-0">BUSCA EN TU CLÓSET PRENDAS QUE MERECEN UNA SEGUNDA OPORTUNIDAD</p>
+                </div>
+
+                <div class="info-steps__item">
+                <div class="info-steps__item-img order-1 order-md-0 mt-4 mt-md-0">
+                    <img src="{{ url('img/info/5.png') }}"  alt="">
+                  </div>
+                  <span class="info-steps__step order-0 order-md-1">3</span>
+                  <p class="info-steps__text order-2 order-md-2 mb-5 mb-md-0">TOMA FOTOS QUE MUESTREN LO MEJOR DE ESA PRENDA</p>
+                </div>
+
+                <div class="info-steps__item">
+                <div class="info-steps__item-img order-1 order-md-0 mt-4 mt-md-0">
+                    <img src="{{ url('img/info/6.png') }}"  alt="">
+                  </div>
+                  <span class="info-steps__step order-0 order-md-1">4</span>
+                  <p class="info-steps__text order-2 order-md-2 mb-5 mb-md-0">PONLE UN PRECIO JUSTO A TU PRENDA, ESO AUMENTA LA RAPIDEZ DE TUS VENTAS</p>
+                </div>
+
+                <div class="info-steps__item">
+                <div class="info-steps__item-img order-1 order-md-0 mt-4 mt-md-0">
+                    <img src="{{ url('img/info/7.png') }}"  alt="">
+                  </div>
+                  <span class="info-steps__step order-0 order-md-1">5</span>
+                  <p class="info-steps__text order-2 order-md-2 mb-5 mb-md-0">VENDE, GANA DINERO Y LLEVA UNA VIDA MÁS SUSTENTABLE</p>
+                </div>
+              </div>
+              <div class="info-steps__footer">
+                <p class="mb-0">www.fashionrecovery.com.mx</p>
+              </div>
+            </div>
+
+            <div class="col-md-12 info-how__container p-0">
+              <img src="{{ url('img/info/como.png') }}" class="info-how" alt="">
+            </div>
             
-            <h5 class="mb-4 TituloFR">Cuida al planeta vendiendo tu Closet</h5>
-
-            <div class="row">
-              <div class="col-sm-4 mb-4">
-                <div class="card">
-                  <img src="..." class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">1. Agrega tus prendas</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  </div>
-                </div>
+            <div class="col-md-12 m-auto info-works">
+              <div class="info-works__header">
+                <h3 class="text-center title-info">
+                  DATOS SOBRE LA <br>
+                  <span class="title-info__strong">INDUSTRIA DE LA MODA</span> 
+                </h3>
               </div>
-              <div class="col-sm-4 mb-4">
-                <div class="card">
-                  <img src="..." class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">2. Envialo con amor</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <div class="info-works__container">
+                <div class="row info-works__item">
+                  <div class="col-md-3 text-center info-works__img-container">
+                    <img src="{{ url('img/info/8.png') }}" class="info-works__1" alt="">
+                  </div>
+                  <div class="col-md-9 text-center p-md-0 d-flex justify-content-center align-items-center">
+                    <p class="m-0">
+                      Es el <strong>segundo consumidor de agua</strong> a nivel mundial. <br>
+                      Genera alrededor de <b>20%</b> de las aguas residuales. <br>
+                      Libera <b>medio millón de toneladas</b> de microfibras sintéticas al océano cada año.
+                    </p>
                   </div>
                 </div>
-              </div>
-              <div class="col-sm-4 mb-4">
-                <div class="card">
-                  <img src="..." class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">3. ¡Haz la diferencia!</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  </div>
-                </div>
-              </div>
-            </div>
 
-            <div class="w-100 text-right mb-4">
-              <a href="{{ url('/item') }}" class="btn btn-fr" role="button">Vende tu Closet</a>
-            </div>
-
-            <hr class="my-5">
-
-            <h5 class="mb-4 TituloFR">Súmate adquiriendo prendas únicas</h5>
-
-            <div class="row">
-              <div class="col-sm-4 mb-4">
-                <div class="card">
-                  <img src="..." class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">1. Busca tu prenda ideal</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <div class="row info-works__item">
+                  <div class="col-md-9 text-center p-md-0 d-flex justify-content-center align-items-center order-2 order-md-1">
+                    <p class="m-0 ">
+                      El consumidor promedio compra 60% más prendas de ropa que hace 15 años y cada artículo se
+                      conserva la mitad del tiempo.
+                    </p>
+                  </div>
+                  <div class="col-md-3 text-center info-works__img-container order-1 order-md-2">
+                    <img src="{{ url('img/info/9.png') }}" class="info-works__2" alt="">
                   </div>
                 </div>
-              </div>
-              <div class="col-sm-4 mb-4">
-                <div class="card">
-                  <img src="..." class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">2. Adquiere la prenda</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                <div class="row info-works__item">
+                  <div class="col-md-3 text-center info-works__img-container">
+                    <img src="{{ url('img/info/10.png') }}" class="info-works__1" alt="">
+                  </div>
+                  <div class="col-md-9 text-center p-md-0 d-flex justify-content-center align-items-center">
+                    <p class="m-0">
+                      Cada año la industria pierde cerca de US$ 500 mil millones en valor de la ropa de temporada que
+                      no se vende y que es arrojada a vertederos en lugar de ser reciclada.
+                    </p>
                   </div>
                 </div>
-              </div>
-              <div class="col-sm-4 mb-4">
-                <div class="card">
-                  <img src="..." class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">3. ¡Dale un segundo uso!</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                <div class="row info-works__item b-0">
+                  <div class="col-md-9 text-center p-md-0 d-flex justify-content-center align-items-center order-2 order-md-1">
+                    <p class="m-0">
+                    La industria produce de 8% a 10% de las emisiones globales de carbono, más que todo el
+  transporte marítimo y los vuelos internacionales combinados.
+                    </p>
                   </div>
-                </div>
+                  <div class="col-md-3 text-center info-works__img-container order-1 order-md-2">
+                    <img src="{{ url('img/info/11.png') }}" class="info-works__2" alt="">
+                  </div>
+                </div>              
               </div>
             </div>
-
-            <div class="w-100 text-right">
-              <a href="{{ url('/') }}" class="btn btn-fr" role="button">Buscar prendas</a>
+            <div class="info-what col-md-12 m-auto">
+              ¿QUÉ PODEMOS HACER?
             </div>
-        </div>
+
+            <div class="col-md-12 m-auto info-card__container">
+
+              <div class="row">
+                <div class="col-md-4 info-card">
+                  <span class="info-card__title">
+                    REDUCE
+                  </span>
+                  <p class="info-card__text">El consumo de prendas que no necesitas realmente.</p>
+                </div>
+                <div class="col-md-4 info-card">
+                  <span class="info-card__title">
+                    REPARA
+                  </span>
+                  <p class="info-card__text">La ropa y accesorios que aún tienen una vida útil.</p>
+                </div>
+                <div class="col-md-4 info-card">
+                  <span class="info-card__title">
+                    RECICLA 
+                  </span>
+                  <p class="info-card__text">Lo que ya no se puede utilizar, pero puede darle vida a algo nuevo.</p>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-4 info-card">
+                  <span class="info-card__title">
+                  REUTILIZA
+                  </span>
+                  <p class="info-card__text">Las prendas a las que has dado poco uso y considéralas antes de comprar.</p>
+                </div>
+                <div class="col-md-4 info-card order-1 order-md-0">
+                  <img src="{{ url('img/info/12.png') }}" alt="" class="info-card__img ">
+                </div>
+                <div class="col-md-4 info-card">
+                  <span class="info-card__title">
+                  REINVENTA 
+                  </span>
+                  <p class="info-card__text">Lo que puede adaptarse a las nuevas tendencias.</p>
+                </div>
+              </div>
+
+              <p class="info-card__footer mt-4 mt-md-0">
+                ¡VISTE VERDE!
+              </p>
+            </div>
+            <div class="col-md-12 m-auto info-footer">
+              <img src="{{ url('img/header/transparent_logo.png') }}" class="info-footer__logo" alt="">
+              <p class="info-footer__web mb-0">
+                <strong>www.fashionrecovery.com.mx</strong>
+              </p>
+              <p class="info-footer__p">
+                Fuente: United Nations Environment Programme. (2019). La Alianza de la ONU para la Moda
+                Sostenible abordará el impacto de la &quot;moda rápida&quot;. Nairobi, Kenia.: Comunicado de prensa.
+              </p>
+            </div>
+
+
+
+
+
       </div>
     </main>
 @endsection
