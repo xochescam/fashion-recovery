9cf6d5548f13ea4e47ebf5fb64a7e001f9f7d4a0
Xochitl Escamilla <xochissea@gmail.com>
2020-12-07 19:23:23 -0600

    Opcion de limpiar filtros

--- resources/js/components/search/Refinament.vue
+++ resources/js/components/search/Refinament.vue
@@ -1,6 +1,19 @@
 <template>
     <div class="col col-md-2 d-block pl-md-0">
 
+        <ais-clear-refinements>
+            <div slot-scope="{ canRefine, refine, createURL }">
+                <a
+                class="btn btn-outline-green"
+                :href="createURL()"
+                @click.prevent="refine"
+                v-if="canRefine"
+                >
+                Limpiar búsqueda
+                </a>
+            </div>
+            </ais-clear-refinements>
+
         <a class="btn btn-outline-green mb-3 w-100 d-block d-md-none" 
             data-toggle="collapse" 
             href="#filtersCollapse" 
@@ -9,11 +22,14 @@
             aria-controls="filtersCollapse">
             Filtrar</a>
 
+
+
         <div class="collapse d-md-block" id="filtersCollapse">
             <div class="card card-body mb-4 p-md-0 border-md-0">
                 <ais-panel>
                     <h6 class="font-weight-bold mt-3">DEPARTAMENTO</h6>
                     <template slot="default" >
+                        
                         <ais-refinement-list
                             attribute="DepName"
                             :searchable="false"
